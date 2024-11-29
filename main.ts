71ee2927d56dbabcd0cc4a3cf6edc9dab92819ce
OKYESMATE <lochlanm@spkealba.catholic.edu.au>
2024-11-30 10:13:37 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+// This will make the microbit into monitor and control mode
+bluetooth.startAccelerometerService()
+bluetooth.startButtonService()
+bluetooth.startIOPinService()
+bluetooth.startLEDService()
+bluetooth.startTemperatureService()
+bluetooth.startMagnetometerService()
